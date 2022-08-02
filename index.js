41a569f188cdea12fa986775170bd92283e87114
tylerj <tylerjcagle@gmail.com>
2022-08-02 16:49:38 -0600

    implemented spotify package

--- index.js
+++ index.js
@@ -1,14 +1,79 @@
-const http = require('http');
+const express = require("express");
+require('dotenv').config()
+const app = express();
 
-const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+var SpotifyWebApi = require("spotify-web-api-node");
+
+var spotifyApi = new SpotifyWebApi({
+  clientId: process.env.SPOTIFY_CLIENT_ID,
+  clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+// Retrieve an access token.
+const accessToken = spotifyApi.getAccessToken();
+if (!accessToken) {
+  spotifyApi.clientCredentialsGrant().then(
+    function (data) {
+      // Save the access token so that it's used in future calls
+      spotifyApi.setAccessToken(data.body["access_token"]);
+    },
+    function (err) {
+      console.log("Something went wrong when retrieving an access token", err);
+    }
+  );
+}
+
+// Get Artist results from a search query
+app.get("/search/:query", (req, res) => {
+  const query = req.params.query;
+  // Search artists whose name contains query params
+  spotifyApi.searchArtists(query).then(
+    function (data) {
+      console.log('Search artists by "Love"', data.body);
+      res.send(data.body);
+    },
+    function (err) {
+      console.error(err);
+    }
+  );
+});
+
+// Get albums from artist id
+app.get("/artistAlbums/:id", (req, res) => {
+  const id = req.params.id;
+  // get artist's albums
+  spotifyApi.getArtistAlbums(id).then(
+    function (data) {
+      console.log("Artist albums", data.body);
+      res.send(data.body);
+    },
+    function (err) {
+      console.error(err);
+    }
+  );
+});
+
+// Get tracks from album
+app.get("/albumTracks/:id", (req, res) => {
+  const id = req.params.id;
+  // Get tracks in an album
+  spotifyApi.getAlbumTracks(id, { limit: 25, offset: 0 }).then(
+    function (data) {
+      console.log(data.body);
+      res.send(data.body);
+    },
+    function (err) {
+      console.log("Something went wrong!", err);
+    }
+  );
+});
+
+app.listen(port, (error) => {
+  if (error) {
+    console.log("Couldn't start server", error);
+  } else {
+    console.log(`Server running at ${port}`);
+  }
 });
