97d8da6a6ac5e9819570e7e1188808c49133dbe8
tylerj <tylerjcagle@gmail.com>
2022-08-02 17:05:13 -0600

    updated to heroku port

--- index.js
+++ index.js
@@ -74,11 +74,8 @@ app.get("/albumTracks/:id", (req, res) => {
   );
 });
 
-app.listen(port, (error) => {
-  if (error) {
-    console.log("Couldn't start server", error);
-  } else {
-    console.log(`Server running at ${port}`);
-  }
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
+
 app.use(favicon(path.join(__dirname,'public','images','favicon.ico')));
